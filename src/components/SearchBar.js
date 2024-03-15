6c8f9fce4ccef050d7198c32aa05d351a33e1cae
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-15 14:37:51 +0100

    new changes made

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 function SearchBar ({onSubmit}){
+    const [term, setTerm] = useState('');
     const handleFormSubmit = (event) =>{
         event.preventDefault();
-        onSubmit('cars');
+        // onSubmit(
+        //     //  never ever ever do this
+        //     // document.querySelector('input').value
+        // );
+
+        onSubmit('');
+    };
+    const handleChange = (event) =>{
+        console.log(event.target.value);
+
     };
    
     return( <div> 
         <form onSubmit={handleFormSubmit}>
 
         
-        <input/>
+        <input onChange={handleChange}/>
         </form>
          
     </div>);
