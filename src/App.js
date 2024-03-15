6e986695b8c8dda7dff595b5a98dc2cfd5999d0c
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-15 16:52:46 +0100

    new changes made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import searchImages from "./api";
+import ImageList from './components/ImageList';
 import SearchBar from "./components/SearchBar";
 
 
 function App (){
-    const handleSubmit =(term) =>{
-        searchImages(term);
+    const handleSubmit =async (term) =>{
+      await  searchImages(term);
        
     };
     return <div>
         <SearchBar onSubmit = {handleSubmit} />
+        <ImageList  />
 
     </div>
 }
